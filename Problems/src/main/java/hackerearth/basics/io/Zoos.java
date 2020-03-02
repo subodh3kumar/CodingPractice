8b383adeeb7992a15b064e9f57b838bb7489ab9f
subodh3kumar <subodh3kumar@gmail.com>
2020-03-02 21:36:40 +0530

    Zoos class updated

--- Problems/src/main/java/hackerearth/basics/io/Zoos.java
+++ Problems/src/main/java/hackerearth/basics/io/Zoos.java
@@ -1,8 +1,34 @@
 package hackerearth.basics.io;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Zoos {
 
     public static void main(String[] args) {
-        
+        String word = getInput();
+        Map<Character, Integer> map = getCharCounts(word);
+        System.out.println(map);
+    }
+
+    private static String getInput() {
+        Scanner input = new Scanner(System.in);
+        String word = input.next();
+        input.close();
+        return word;
+    }
+
+    private static Map<Character, Integer> getCharCounts(String word) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < word.length(); i++) {
+            if (map.containsKey(word.charAt(i))) {
+                int value = map.get(word.charAt(i));
+                map.put(word.charAt(i), ++value);
+            } else {
+                map.put(word.charAt(i), 1);
+            }
+        }
+        return map;
     }
 }
