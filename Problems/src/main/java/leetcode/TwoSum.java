6311b1c306117f6a6c488521e76a75242aa0cdee
subodh3kumar <subodh3kumar@gmail.com>
2020-03-08 12:08:38 +0530

    adding TwoSum.java

--- Problems/src/main/java/leetcode/TwoSum.java
+++ Problems/src/main/java/leetcode/TwoSum.java
@@ -1,5 +1,28 @@
 package leetcode;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class TwoSum {
 
+    public static void main(String[] args) throws Exception {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            int[] nums = {2, 7, 11, 15, 6, 9, 15, 4, 3};
+            int target = Integer.parseInt(reader.readLine());
+            int[] index = twoSum(nums, target);
+            System.out.println(Arrays.toString(index));
+        }
+    }
+
+    private static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
 }
