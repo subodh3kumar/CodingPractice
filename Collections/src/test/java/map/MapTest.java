bdacfe78afd7bbcb49cefa2aa2be024990dbd50a
subodh3kumar <subodh3kumar@gmail.com>
2020-07-19 20:45:15 +0530

    MapTest.java

--- Collections/src/test/java/map/MapTest.java
+++ Collections/src/test/java/map/MapTest.java
@@ -1,15 +1,20 @@
 package map;
 
+import org.junit.jupiter.api.Test;
+
 import java.util.HashMap;
 import java.util.Map;
 
-public class DuplicateKeyInsertionHashMap {
+public class MapTest {
 
-    public static void main(String[] args) {
+    @Test
+    public void testMapDuplicateKey() {
         Map<String, Integer> map = new HashMap<>();
 
         map.put("ONE", 1);
         map.put("ONE", 100);
+
+        System.out.println(map);
 
         map.put(null, null);
         map.put(null, 200);
