3fde28bf9c0eaf14136a4344f129d454ba202f90
subodh3kumar <subodh3kumar@gmail.com>
2020-07-30 22:31:48 +0530

    date time

--- Strings/src/test/java/CharAtMethodTest.java
+++ Strings/src/test/java/CharAtMethodTest.java
@@ -1,5 +1,3 @@
-package methods;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
--- Strings/src/test/java/CodePointMethodTest.java
+++ Strings/src/test/java/CodePointMethodTest.java
@@ -1,5 +1,3 @@
-package methods;
-
 import org.junit.jupiter.api.Test;
 
 public class CodePointMethodTest {
