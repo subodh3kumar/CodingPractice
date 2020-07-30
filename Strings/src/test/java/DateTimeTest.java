48d2dbedb9684f17047201d1f45cff026a7b84ec
subodh3kumar <subodh3kumar@gmail.com>
2020-07-30 22:30:04 +0530

    date time

---
+++ Strings/src/test/java/DateTimeTest.java
@@ -0,0 +1,60 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DateTimeTest {
+
+    private DateTime obj;
+
+    @BeforeEach
+    private void init() {
+        obj = new DateTime();
+    }
+
+    @Test
+    public void parseDate() {
+        LocalDate localDate = obj.parse("07/30/2020", "MM/dd/yyyy");
+        System.out.println("local date: " + localDate);
+        assertThat(localDate).isEqualTo(LocalDate.of(2020, 07, 30));
+    }
+
+    @Test
+    public void testDayDifferenceUsingChronoUtil() {
+        LocalDate previousDay = obj.parse("07/25/2020", "MM/dd/yyyy");
+        LocalDate today = LocalDate.now();
+        long days = ChronoUnit.DAYS.between(previousDay, today);
+        System.out.println("day difference: " + days);
+        assertThat(days).isEqualTo(5);
+    }
+
+    @Test
+    public void testDayDifferenceUsingDuration() {
+        LocalDate oldDate = obj.parse("20-07-2020", "dd-MM-yyyy");
+        LocalDate today = LocalDate.now();
+        Duration duration = Duration.between(oldDate.atStartOfDay(), today.atStartOfDay());
+        long days = duration.toDays();
+        System.out.println("day difference: " + days);
+    }
+
+    @Test
+    public void testDayDifferenceUsingUntil() {
+        LocalDate oldDate = obj.parse("12-07-2020", "dd-MM-yyyy");
+        LocalDate today = LocalDate.now();
+        Period until = oldDate.until(today);
+        System.out.println("day difference: " + until.getDays());
+    }
+
+    @Test
+    public void testDayDifferenceUsingUntilChrono() {
+        LocalDate oldDate = obj.parse("18-07-2020", "dd-MM-yyyy");
+        LocalDate today = LocalDate.now();
+        long until = oldDate.until(today, ChronoUnit.DAYS);
+        System.out.println("day difference: " + until);
+    }
+}
