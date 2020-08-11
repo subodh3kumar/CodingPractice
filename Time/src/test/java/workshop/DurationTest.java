d8055db0419fc43343fd30c4021bfa12970bbdd0
subodh3kumar <subodh3kumar@gmail.com>
2020-08-11 22:49:51 +0530

    time

---
+++ Time/src/test/java/workshop/DurationTest.java
@@ -0,0 +1,21 @@
+package workshop;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.Instant;
+
+public class DurationTest {
+
+    @Test
+    public void durationInSecAndMillis() {
+        Instant start = Instant.now();
+        try {
+            Thread.sleep(2024);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Duration duration = Duration.between(start, Instant.now());
+        System.out.printf("%d seconds and %d millisecond", duration.toSeconds(), duration.toMillis());
+    }
+}
