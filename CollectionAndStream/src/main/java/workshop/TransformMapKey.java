f02f790c4798cb9780888e6fa6a059c73187720b
Subodh Kumar <subodh3kumar@gmail.com>
2022-11-06 22:34:41 +0530

    CodingPractice

--- CollectionAndStream/src/main/java/workshop/TransformMapKey.java
+++ CollectionAndStream/src/main/java/workshop/TransformMapKey.java
@@ -20,25 +20,25 @@ public class TransformMapKey {
     }
 
     private static Map<SearchKey, Product> getProductMap() {
-        Map<SearchKey, Product> result = new HashMap<>();
+        Map<SearchKey, Product> map = new HashMap<>();
 
         SearchKey key1 = new SearchKey(101, "Id");
         Product product1 = new Product(101, "Computer", "HP Computer");
-        result.put(key1, product1);
+        map.put(key1, product1);
 
         SearchKey key2 = new SearchKey(102, "Id");
         Product product2 = new Product(102, "Book", "Computer Book");
-        result.put(key2, product2);
+        map.put(key2, product2);
 
         SearchKey key3 = new SearchKey(103, "Id");
         Product product3 = new Product(103, "Fan", "Ceiling fan");
-        result.put(key3, product3);
+        map.put(key3, product3);
 
         SearchKey key4 = new SearchKey(104, "Id");
         Product product4 = new Product(104, "Table", "Wooden table");
-        result.put(key4, product4);
+        map.put(key4, product4);
 
-        return result;
+        return map;
     }
 
     private record SearchKey(int id, String idType) {
---
+++ Concurrency/src/main/java/workshop/LongAdderTest.java
@@ -0,0 +1,16 @@
+package workshop;
+
+import java.util.concurrent.atomic.LongAdder;
+
+public class LongAdderTest {
+
+    public static void main(String[] args) {
+
+        LongAdder counter = new LongAdder();
+
+        Runnable task = counter::increment;
+        task.run();
+
+        System.out.println("counter: " + counter);
+    }
+}
