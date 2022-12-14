42e04165cbb94b3914d93223066cbcabe05ade3b
Subodh Kumar <subodh3kumar@gmail.com>
2022-12-15 01:33:53 +0530

    Misc

---
+++ Misc/src/main/java/org/example/FlatMapTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.stream.Stream;
+
+public class FlatMapTest {
+
+    static Stream<String> addPrefix(String prefix) {
+        return Stream.of("java", "duke")
+                .map(str -> prefix + " " + str);
+    }
+
+    public static void main(String[] args) {
+        var message = Stream.of("hey", "hi")
+                .flatMap(FlatMapTest::addPrefix)
+                .toList();
+        System.out.println(message);
+    }
+}
