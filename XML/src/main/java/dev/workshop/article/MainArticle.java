3a48e2e2a12e0f8cab3d5fa01b4b2f00acd541d1
Subodh Kumar <subodh3kumar@gmail.com>
2024-03-22 23:29:18 +0530

    remove namespace prefix

--- XML/src/main/java/dev/workshop/article/MainArticle.java
+++ XML/src/main/java/dev/workshop/article/MainArticle.java
@@ -13,12 +13,17 @@ public class MainArticle {
 
         String xml = Files.readString(Path.of("src/main/resources/articles.xml"));
         xml = removeXmlStringNamespaceAndPreamble(xml);
-        System.out.println("xml: " + xml);
+        //System.out.println("xml: " + xml);
+        //xml = xml.replaceAll("\\r\\n\\s+", "");
+        //xml = xml.replaceAll("[ \t\n\r]*", "");
+        //xml = xml.replaceAll("[ \\t\\n\\r]+", "");
+        xml = xml.replaceAll("[ \t\n\r]", "");
+        System.out.println(xml);
     }
 
     public static String removeXmlStringNamespaceAndPreamble(String xmlString) {
         return xmlString.replaceAll("(<\\?[^<]*\\?>)?", "") /* remove preamble */
-                .replaceAll("xmlns.*?(\"|\').*?(\"|\')", "") /* remove xmlns declaration */
+                .replaceAll("\\s+xmlns.*?([\"']).*?(\"|\')", "") /* remove xmlns declaration */
                 .replaceAll("(<)(\\w+:)(.*?>)", "$1$3") /* remove opening tag prefix */
                 .replaceAll("(</)(\\w+:)(.*?>)", "$1$3"); /* remove closing tags prefix */
     }
