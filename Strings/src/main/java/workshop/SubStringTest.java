4584a180b3cfe68b65e5e1ac921adb3e11322eef
Subodh Kumar <subodh3kumar@gmail.com>
2024-03-30 17:26:15 +0530

    test string repetition

--- Strings/src/main/java/workshop/SubStringTest.java
+++ Strings/src/main/java/workshop/SubStringTest.java
@@ -1,7 +1,12 @@
 package workshop;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class SubStringTest {
 
@@ -22,10 +27,25 @@ public class SubStringTest {
     }
 
     @Test
+    @Disabled
     public void testRepeat() {
         String placeholder = "?, ";
         String repeat = placeholder.repeat(21).strip();
         repeat = repeat.substring(0, repeat.length() - 1);
         System.out.println(repeat);
+    }
+
+    @Test
+    public void testStringRepetitionUsingGenerate() {
+        String collect = Stream.generate(() -> "?")
+                .limit(3)
+                .collect(Collectors.joining(", "));
+        Assertions.assertEquals("?, ?, ?", collect);
+    }
+
+    @Test
+    public void testStringRepetitionUsingJoin() {
+        String join = String.join(", ", Collections.nCopies(3, "?"));
+        Assertions.assertEquals("?, ?, ?", join);
     }
 }
